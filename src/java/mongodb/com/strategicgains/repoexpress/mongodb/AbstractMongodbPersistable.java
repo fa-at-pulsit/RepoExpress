a6fbf0c71b14eaf146bd59fd5a5d6fb94bc8c752
Todd Fredrich <tfredrich@gmail.com>
2011-10-28 12:17:40 -0600

    Introduced AbstractTimestampedIdentifiable and AbstractMongodbPersistable.

---
+++ src/java/mongodb/com/strategicgains/repoexpress/mongodb/AbstractMongodbPersistable.java
@@ -0,0 +1,49 @@
+/*
+    Copyright 2011, Strategic Gains, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+package com.strategicgains.repoexpress.mongodb;
+
+import org.bson.types.ObjectId;
+
+import com.google.code.morphia.annotations.Id;
+import com.strategicgains.repoexpress.domain.AbstractTimestampedIdentifiable;
+
+/**
+ * @author toddf
+ * @since Oct 27, 2011
+ */
+public abstract class AbstractMongodbPersistable
+extends AbstractTimestampedIdentifiable
+{
+	@Id
+	private ObjectId id;
+
+	@Override
+	public String getId()
+	{
+		return (id == null ? null : id.toString());
+	}
+
+	@Override
+	public void setId(String idString)
+	{
+		this.id = (idString ==null ? null : new ObjectId(idString));
+	}
+	
+	public ObjectId getObjectId()
+	{
+		return id;
+	}
+}
