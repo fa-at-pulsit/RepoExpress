faaa41fb6fa81209173cd650fb2909ede635a292
Todd Fredrich <tfredrich@gmail.com>
2013-04-12 15:41:20 -0600

    Very rudimentary cassandra beginnings.

---
+++ cassandra/src/main/java/com/strategicgains/repoexpress/cassandra/AbstractCassandraEntity.java
@@ -0,0 +1,62 @@
+package com.strategicgains.repoexpress.cassandra;
+/*
+    Copyright 2013, Strategic Gains, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+import java.util.UUID;
+
+import com.strategicgains.repoexpress.domain.AbstractTimestampedIdentifiable;
+import com.strategicgains.repoexpress.domain.UuidIdentifiable;
+
+/**
+ * A Cassandra entity that is identified by a UUID as its primary identifier.
+ * It also has createdAt and updatedAt properties.
+ * 
+ * @author toddf
+ * @since Mar 18, 2013
+ */
+public abstract class AbstractCassandraEntity
+extends AbstractTimestampedIdentifiable
+implements UuidIdentifiable
+{
+	private UUID id;
+
+	@Override
+	public String getId()
+	{
+//		return (id == null ? null : UuidConverter.format(id));
+		return (id == null ? null : id.toString());
+	}
+
+	@Override
+	public void setId(String idString)
+	{
+//		this.id = (idString ==null ? null : UuidConverter.parse(idString));
+		this.id = (idString ==null ? null : UUID.fromString(idString));
+	}
+
+	@Override
+    public UUID getUuid()
+    {
+	    return id;
+    }
+
+	@Override
+    public void setUuid(UUID uuid)
+    {
+		this.id = uuid;
+    }
+}
---
+++ cassandra/src/main/java/com/strategicgains/repoexpress/cassandra/CassandraEntityRepository.java
@@ -0,0 +1,54 @@
+package com.strategicgains.repoexpress.cassandra;
+/*
+    Copyright 2013, Strategic Gains, Inc.
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+
+import java.util.UUID;
+
+import com.strategicgains.repoexpress.adapter.UuidAdapter;
+import com.strategicgains.repoexpress.event.DefaultTimestampedIdentifiableRepositoryObserver;
+import com.strategicgains.repoexpress.event.UuidIdentityRepositoryObserver;
+
+/**
+ * A Cassandra repository that manages types of AbstractCassandraEntity, which are
+ * identified by UUIDs. It utilizes the {@link UuidIdentityRepositoryObserver} to assign
+ * a UUID on creation.  It also uses {@link DefaultTimestampedIdentifiableRepositoryObserver}
+ * to set the createAt and updatedAt dates on the object as appropriate.
+ * <p/>
+ * Storing a UUID as the ID (as this repository does) requires four (4) bytes for the ID.
+ * <p/>
+ * To implement single-table inheritance, simply pass in all the sub-classes that
+ * exist in this collection, with the inheritance-root listed first.
+ * 
+ * @author toddf
+ * @since Apr 12, 2013
+ */
+public class CassandraEntityRepository<T extends AbstractCassandraEntity>
+extends CassandraRepository<T, UUID>
+{
+    public CassandraEntityRepository(CassandraConfiguration config, String databaseName, Class<? extends T>... types)
+    {
+	    super(config, databaseName, types);
+	    initializeObservers();
+	    setIdentifierAdapter(new UuidAdapter());
+    }
+
+    protected void initializeObservers()
+    {
+		addObserver(new DefaultTimestampedIdentifiableRepositoryObserver<T>());
+		addObserver(new UuidIdentityRepositoryObserver<T>());
+    }
+}
